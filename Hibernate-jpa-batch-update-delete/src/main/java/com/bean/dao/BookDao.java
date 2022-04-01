28bc5ba3651dc520c96feabba40aa3e90101182c
Ankush <ankush.bharambe@coditas.com>
2022-04-01 14:59:00 +0530

    april 1

---
+++ Hibernate-jpa-batch-update-delete/src/main/java/com/bean/dao/BookDao.java
@@ -0,0 +1,106 @@
+package com.bean.dao;
+
+import com.bean.Teacher;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.Arrays;
+import java.util.List;
+
+public class BookDao {
+    static EntityManagerFactory factory = Persistence.createEntityManagerFactory("entity");
+    static EntityManager manager = factory.createEntityManager();
+    static CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+
+    public static void FetchAll() {
+        CriteriaQuery<Teacher> criteriaQuery = criteriaBuilder.createQuery(Teacher.class);
+        Root<Teacher> from = criteriaQuery.from(Teacher.class);
+        CriteriaQuery<Teacher> select = criteriaQuery.select(from);
+        TypedQuery<Teacher> allQuery = manager.createQuery(select);
+        List<Teacher> list = allQuery.getResultList();
+
+        for(Teacher t : list) {
+            System.out.println(t);
+        }
+    }
+
+    public static void SelectionByNameAndDesignation() {
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Teacher> from = criteriaQuery.from(Teacher.class);
+        criteriaQuery.multiselect(from.get("name"), from.get("desig"));
+        List<Object[]> list = manager.createQuery(criteriaQuery).getResultList();
+
+        for(Object[] obj : list) {
+            System.out.println(Arrays.toString(obj));
+        }
+    }
+
+    public static void FetchOrderBySalary() {
+        CriteriaQuery<Teacher> criteriaQuery = criteriaBuilder.createQuery(Teacher.class);
+        Root<Teacher> from = criteriaQuery.from(Teacher.class);
+        CriteriaQuery<Teacher> select = criteriaQuery.select(from);
+
+        criteriaQuery.orderBy(criteriaBuilder.asc(from.get("salary")));
+        List<Teacher> list = manager.createQuery(criteriaQuery).getResultList();
+
+        for(Teacher t : list) {
+            System.out.println(t.getSalary());
+        }
+    }
+
+    public static void minTeacher(){
+
+        Query query = manager.createQuery("select MIN(e.salary) from Teacher e");
+        double res = (double)query.getSingleResult();
+        System.out.println("res is"+res);
+
+
+    }
+    public static void TeacherA(){
+        Query query = manager.createQuery("Select t from Teacher t where t.name like 'A%'");
+        List<Teacher> list1 = (List<Teacher>) query.getResultList();
+        for(Teacher t:list1)
+        {
+            System.out.println("Teacher name: " +t.getName());
+        }
+
+    }
+    public static void Teacherbetn(){
+
+        Query query = manager.createQuery("Select t from Teacher t where t.salary between 10000 and 25000");
+        List<Teacher> list1 = (List<Teacher>) query.getResultList();
+        for(Teacher t:list1)
+        {
+            System.out.println(t.getName() + " Salary is " + t.getSalary());
+        }
+
+
+    }
+
+    public  static void updateSalary(int salary){
+    Query query = (Query) manager.createQuery("Select e from Teacher e where e.salary > 15000");
+    List<Teacher> list = query.getResultList();
+        for (Teacher t : list) {
+        manager.getTransaction().begin();
+        t.setSalary(t.getSalary() +salary );
+        manager.getTransaction().commit();
+        System.out.println(t);
+    }
+    }
+    public static void batchDelete(int salary) {
+        manager.getTransaction().begin();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaDelete<Teacher> delete = builder.createCriteriaDelete(Teacher.class);
+
+        Root e = delete.from(Teacher.class);
+        delete.where(builder.equal(e.get("salary"), salary));
+        manager.createQuery(delete).executeUpdate();
+        manager.getTransaction().commit();
+        System.out.println("Delete Successful................");
+    }
+
+
+}
